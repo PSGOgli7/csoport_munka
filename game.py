fcfea864253a4fe843147cbef005574a6cb00971
xXImrePVPXx <118882031+xXImrePVPXx@users.noreply.github.com>
2023-03-01 07:52:59 +0100

    asd

--- game.py
+++ game.py
@@ -5,6 +5,6 @@ with open("cards.json", "r", encoding = "utf-8") as f:
 with open("entity.py", "r", encoding = "utf-8") as p:
     print(p)
 
-print(cards)
+print(cards)#"Tovább" = merre megy tovább a játék
 for e in cards:
     print(e["Sorszám"])
