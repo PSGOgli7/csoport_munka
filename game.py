e9e67f7680c09e8d6317323af61edbe7269c2417
xXImrePVPXx <118882031+xXImrePVPXx@users.noreply.github.com>
2023-03-02 21:19:53 +0100

    Játék nagyon nagyon nagyon alapja

--- game.py
+++ game.py
@@ -1,10 +1,14 @@
 import json
-import keyboard
 with open("cards.json", "r", encoding = "utf-8") as f:
     cards = json.load(f)
 with open("entity.py", "r", encoding = "utf-8") as p:
     print(p)
 
-print(cards)#"Tovább" = merre megy tovább a játék
-for e in cards:
-    print(e["Sorszám"])
+json = cards
+sorszam = 1
+while True:
+  print(json[sorszam-1]['Szöveg'])
+  # válasz lehetőségek kiírása json[sorszam-1]['Tovabb'] alapján
+  valasz = int(input("Válassz: "))
+  #válasz ellenőrzése, hogy benne van-e a továbban
+  sorszam = json[sorszam-1]['Tovabb'][valasz]
