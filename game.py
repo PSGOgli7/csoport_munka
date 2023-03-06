c21d3138e592aac07ba08491536da02627d54a80
xXImrePVPXx <118882031+xXImrePVPXx@users.noreply.github.com>
2023-03-06 19:41:54 +0100

    Harc használata, Halál és Győzelem.
    Beillesztettem a harcot ill. hogy tudja tárolni a json a mobokat(ha nincs mob, nem kell a "Mobs" lista) ill. most még max 2 mob lehet csak, persze ezt lehet majd növelni. 😘💀

--- game.py
+++ game.py
@@ -1,14 +1,40 @@
 import json
+import random
+from entity import Entity
+
 with open("cards.json", "r", encoding = "utf-8") as f:
     cards = json.load(f)
 with open("entity.py", "r", encoding = "utf-8") as p:
     print(p)
+player = Entity("sanyi")
 
 json = cards
 sorszam = 1
 while True:
   print(json[sorszam-1]['Szöveg'])
+  if json[sorszam-1]['Esemény'] == 'Tamad':
+      mobj = json[sorszam-1]['Mobs']['Mob1']
+      mob = Entity(mobj[0], mobj[1], mobj[2], mobj[3])
+      if "Mob2" in json[sorszam-1]['Mobs']:
+          mobj2 = json[sorszam - 1]['Mobs']['Mob2']
+          mob2 = Entity(mobj2[0], mobj2[1], mobj2[2], mobj[3])
+          if not player.attack(mob2):
+              break
+      if not player.attack(mob):
+         break
+      else:
+          valasz = int(input("Válassz: "))
+          if len(json[sorszam - 1]['Tovabb']) <= valasz or valasz == 0:
+              print('Ez nem opció.')
+              continue
+          sorszam = json[sorszam - 1]['Tovabb'][valasz]
   # válasz lehetőségek kiírása json[sorszam-1]['Tovabb'] alapján
-  valasz = int(input("Válassz: "))
+  elif json[sorszam-1]['Esemény'] == 'Ugras':
+      valasz = int(input("Válassz: "))
+      if len(json[sorszam - 1]['Tovabb']) <= valasz or valasz == 0:
+          print('Ez nem opció.')
+          continue
+      sorszam = json[sorszam - 1]['Tovabb'][valasz]
   #válasz ellenőrzése, hogy benne van-e a továbban
-  sorszam = json[sorszam-1]['Tovabb'][valasz]
+
+print('Vége a játéknak!')
